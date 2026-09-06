1398b010640b4d06f37c812489fa4b718a645aef
R-Jay-Hub <raveeshajayasinghe12@gmail.com>
2026-09-06 12:50:09 +0530

    feat(api): expose persistent tasks analytics

--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import * as controller from '../controllers/taskController.js'
 import { validate } from '../middleware/validate.js'
-import { createTaskSchema, taskIdSchema, taskQuerySchema, updateTaskSchema } from '../schemas/taskSchemas.js'
+import { validateObjectId } from '../middleware/validateObjectId.js'
+import { analyticsQuerySchema, createTaskSchema, taskIdSchema, taskQuerySchema, updateTaskSchema } from '../schemas/taskSchemas.js'
+import { asyncHandler } from '../utils/asyncHandler.js'
 
 const router = Router()
-router.get('/', validate(taskQuerySchema, 'query'), controller.list)
-router.post('/', validate(createTaskSchema), controller.create)
-router.get('/:id', validate(taskIdSchema, 'params'), controller.getOne)
-router.patch('/:id', validate(taskIdSchema, 'params'), validate(updateTaskSchema), controller.update)
-router.delete('/:id', validate(taskIdSchema, 'params'), controller.remove)
+router.get('/analytics/overdue', validate(analyticsQuerySchema, 'query'), asyncHandler(controller.overdueSummary))
+router.get('/', validate(taskQuerySchema, 'query'), asyncHandler(controller.list))
+router.post('/', validate(createTaskSchema), asyncHandler(controller.create))
+router.get('/:id', validateObjectId, validate(taskIdSchema, 'params'), asyncHandler(controller.getOne))
+router.patch('/:id', validateObjectId, validate(taskIdSchema, 'params'), validate(updateTaskSchema), asyncHandler(controller.update))
+router.delete('/:id', validateObjectId, validate(taskIdSchema, 'params'), asyncHandler(controller.remove))
 export default router
